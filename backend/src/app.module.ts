e89caf85108ae8f3bb3b01451904012a9d86034d
Osama Adam <osamaadamme@gmail.com>
2024-02-26 23:00:51 +0200

    creating a dev image for the backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,21 +1,26 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { ApartmentModule } from "./apartment/apartment.module";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
 import { SeederModule } from "./seeder/seeder.module";
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: "postgres",
-      host: "localhost",
-      port: 5432,
-      username: "postgres",
-      password: "postgres",
-      database: "nawy",
-      autoLoadEntities: true,
-      synchronize: true,
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        type: "postgres",
+        host: configService.get<string>("DB_HOST"),
+        port: configService.get<number>("DB_PORT"),
+        username: configService.get<string>("DB_USERNAME"),
+        password: configService.get<string>("DB_PASSWORD"),
+        database: configService.get<string>("DB_DATABASE"),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+      inject: [ConfigService],
     }),
     ApartmentModule,
     SeederModule,
